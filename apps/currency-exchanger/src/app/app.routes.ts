af3be892ea58731adf08f100d614c01400cdd370
Hassan-Gad-nt <Hassan.Gad@neomtech.com>
2023-06-01 09:41:37 +0300

    create home page

--- apps/currency-exchanger/src/app/app.routes.ts
+++ apps/currency-exchanger/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+];
