2e6426e0cb4d328573d77d248168be37a760d7c1
Hassan-Gad-nt <Hassan.Gad@neomtech.com>
2023-06-01 08:39:56 +0300

    add currency converter component

---
+++ libs/currency/src/currency-converter/currency-converter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bm-currency-converter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './currency-converter.component.html',
+  styleUrls: ['./currency-converter.component.scss'],
+})
+export class CurrencyConverterComponent {}
