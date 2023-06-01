f31d8868e27da3ed00d932431d5819535cb817c4
Hassan-Gad-nt <Hassan.Gad@neomtech.com>
2023-06-01 09:51:29 +0300

    create details page component

--- apps/currency-exchanger/src/app/app.routes.ts
+++ apps/currency-exchanger/src/app/app.routes.ts
@@ -5,4 +5,9 @@ export const appRoutes: Route[] = [
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'details',
+    loadComponent: () =>
+      import('./details/details.component').then((m) => m.DetailsComponent),
+  },
 ];
---
+++ apps/currency-exchanger/src/app/details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'bm-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss'],
+})
+export class DetailsComponent {}
