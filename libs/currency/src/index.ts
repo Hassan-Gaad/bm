02fa10c4af9a99b92afabebb2cf4a98d58c36e85
Hassan-Gad-nt <Hassan.Gad@neomtech.com>
2023-06-04 18:42:59 +0300

    replace currency card comp with a tiny template instead

--- libs/currency/src/index.ts
+++ libs/currency/src/index.ts
@@ -1,4 +1,2 @@
 export * from './currency-converter/currency-converter.component';
-export * from './currency-card/currency-card.component';
-
 export * from './currency-chart/currency-chart.component';
